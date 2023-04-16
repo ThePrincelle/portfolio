0ea1ec399278317fb918880446f692349bcbf6b7
Maxime Princelle <maxime@princelle.org>
2023-04-16 18:19:25 +0200

    💄 add logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,13 @@ export default defineNuxtConfig({
       '@nuxt/content',
       '@nuxtjs/color-mode',
       '@nuxtjs/tailwindcss',
+      '@nuxt/image-edge',
       'nuxt-icons'
     ],
     content: {
       // https://content.nuxtjs.org/api/configuration
+    },
+    image: {
+      dir: 'assets/images'
     }
 })
